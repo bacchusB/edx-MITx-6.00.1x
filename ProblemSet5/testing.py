8b6a15aedfa08b1980372d674597e6c1bf70ad92
bacchus <bacchus@bacchuss-MacBook-Air.local>
2018-08-02 17:14:13 +0300

    initial

---
+++ ProblemSet5/testing.py
@@ -0,0 +1,110 @@
+### DO NOT MODIFY THIS FUNCTION ###
+def load_words(file_name):
+    '''
+    file_name (string): the name of the file containing 
+    the list of words to load    
+    
+    Returns: a list of valid words. Words are strings of lowercase letters.
+    
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    '''
+    print('Loading word list from file...')
+    # inFile: file
+    in_file = open(file_name, 'r')
+    # line: string
+    line = in_file.readline()
+    # word_list: list of strings
+    word_list = line.split()
+    print('  ', len(word_list), 'words loaded.')
+    in_file.close()
+    return word_list
+
+WORDLIST_FILENAME = 'words.txt'
+
+class ShiftNumberError:pass
+
+class Message(object):
+    ### DO NOT MODIFY THIS METHOD ###
+    def __init__(self, text):
+        '''
+        Initializes a Message object
+                
+        text (string): the message's text
+
+        a Message object has two attributes:
+            self.message_text (string, determined by input text)
+            self.valid_words (list, determined using helper function load_words
+        '''
+        self.message_text = text
+        self.valid_words = load_words(WORDLIST_FILENAME)
+
+    ### DO NOT MODIFY THIS METHOD ###
+    def get_message_text(self):
+        '''
+        Used to safely access self.message_text outside of the class
+        
+        Returns: self.message_text
+        '''
+        return self.message_text
+
+    ### DO NOT MODIFY THIS METHOD ###
+    def get_valid_words(self):
+        '''
+        Used to safely access a copy of self.valid_words outside of the class
+        
+        Returns: a COPY of self.valid_words
+        '''
+        return self.valid_words[:]
+        
+    def build_shift_dict(self,shift):
+        '''
+        Creates a dictionary that can be used to apply a cipher to a letter.
+        The dictionary maps every uppercase and lowercase letter to a
+        character shifted down the alphabet by the input shift. The dictionary
+        should have 52 keys of all the uppercase letters and all the lowercase
+        letters only.        
+        
+        shift (integer): the amount by which to shift every letter of the 
+        alphabet. 0 <= shift < 26
+
+        Returns: a dictionary mapping a letter (string) to 
+                 another letter (string). 
+        '''
+        import string
+        letters = string.ascii_lowercase
+        
+        if (shift >26) or (shift < 0):
+            raise ShiftNumberError('WTF man?')
+
+        self.shifted_dict = {}
+        for i in range(25):
+            number = (i+shift) if (i+shift)<=25 else i+shift-26
+            self.shifted_dict[letters[i]] = letters[number]
+        return self.shifted_dict
+
+    def apply_shift(self, shift):
+        '''
+        Applies the Caesar Cipher to self.message_text with the input shift.
+        Creates a new string that is self.message_text shifted down the
+        alphabet by some number of characters determined by the input shift        
+        
+        shift (integer): the shift with which to encrypt the message.
+        0 <= shift < 26
+
+        Returns: the message text (string) in which every character is shifted
+             down the alphabet by the input shift
+        '''
+        self.encrypted_line = ''
+        shifted_dict = self.build_shift_dict(shift)
+
+        for letter in self.message_text:
+            if letter.lower() in shifted_dict.keys():
+                if letter.islower():
+                    self.encrypted_line += self.shifted_dict[letter]
+                else:
+                    self.encrypted_line += self.shifted_dict[letter.lower()].upper()
+            else:
+                self.encrypted_line+=letter
+        return self.encrypted_line
+
